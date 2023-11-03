abc3d219bd325118cbf8859c0731362313542302
Laurenne Oliver <laurenneoliver@gmail.com>
2023-11-03 12:18:51 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const tmdbKey = ""; // add key
+const tmdbBaseUrl = "https://api.themoviedb.org/3";
+const playBtn = document.getElementById("playBtn");
+
+const getGenres = async () => {
+  const genreRequestEndpoint = "/genre/movie/list";
+  const requestParams = "?api_key=tmdbKey";
+  const urlToFetch = tmdbBaseUrl + genreRequestEndpoint + requestParams;
+  try {
+    fetch()
+  } catch (Error) {
+    console.log(Error);
+  }
+};
+
+const getMovies = () => {
+  const selectedGenre = getSelectedGenre();
+};
+
+const getMovieInfo = () => {};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById("movieInfo");
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  }
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
